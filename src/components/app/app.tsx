bdf4418901490dbb36ea865c9852a3f24418c32f
Luiza Leahu <leahuluiza@gmail.com>
2023-11-10 10:01:45 +0100

    fix page 404 and added styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,11 +5,6 @@ import { AppRoute } from '../../const';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import BasketPage from '../../pages/basket-page/basket-page';
 import ProductPage from '../../pages/product-page/product-page';
-import Spinner from '../spinner/spinner';
-import { useAppSelector } from '../../hooks';
-import { getErrorStatus } from '../../store/data-process/data-process.selectors';
-import ErrorPage from '../../pages/error-page/error-page';
-import { isProductsStatusLoading } from '../../store/data-process/data-process.selectors';
 import ScrollToTop from '../scroll-to-top/scroll-to-top';
 import browserHistory from '../../browser-history';
 import HistoryRouter from '../history-route/history-route';
@@ -17,20 +12,6 @@ import HistoryRouter from '../history-route/history-route';
 
 function App() {
 
-  const isProductsDataLoading = useAppSelector(isProductsStatusLoading);
-  const hasError = useAppSelector(getErrorStatus);
-
-  if (isProductsDataLoading) {
-    return (
-      <Spinner />
-    );
-  }
-
-  if (hasError) {
-    return (
-      <ErrorPage />
-    );
-  }
 
   return (
     <HelmetProvider>
